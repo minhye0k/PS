c0270c1fa20ebe30d86097b812ffed29e2769ec8
minhyeok <omh34658981@gmail.com>
2023-05-20 12:04:36 +0900

    resolve 11399 in baekjoon

---
+++ baekjoon/greedy/11399.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int N;
+    cin >> N;
+
+    int arr[N];
+
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr+N);
+
+    int sum = arr[0];
+
+    for(int i=1; i<N; i++){
+        arr[i] = arr[i-1] + arr[i];
+        sum += arr[i];
+    }
+
+    cout << sum;
+
+}
